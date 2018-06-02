494683e3d9005426e132179e1b1534cdc9bdd5a3
or <gaizeror@gmail.com>
2018-06-02 15:37:57 +0300

    First init

---
+++ allClasses.cpp
@@ -0,0 +1,278 @@
+#include "allClasses.h"
+
+using namespace std;
+
+//Animal
+Animal::Animal() : m_color("GRAY"), m_childCount(0), m_avgLifetime(0) {};
+Animal::Animal( const char* color, int childs, float avgLifetime )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+};
+
+// TBD
+//Animal::Animal( ifstream& in_file )
+// {
+
+// };
+
+const char* Animal::GetColor() const
+{
+    return m_color;
+};
+int	Animal::GetChildCount() const
+{
+    return m_childCount;
+};
+float Animal::GetLifetime() const
+{
+    return m_avgLifetime;
+};
+
+//Mamal
+Mammals::Mammals() : m_milkLiters(0), m_pregnancyTime(0) {};
+
+Mammals::Mammals( const char* color, int childs, float avgLifetime, float preg, float milk )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_pregnancyTime = preg;
+    m_milkLiters = milk;
+};
+
+//TBD
+// Mammals::Mammals( ifstream& in_file )
+// {
+    
+// };//init the Mammals from a binary file
+
+float Mammals::GetPregnanceTime() const
+{
+    return m_pregnancyTime;
+};
+
+
+float Mammals::GetMilk() const
+{
+    return m_milkLiters;
+};
+
+Birds::Birds() : m_incubationTime(0) {};
+
+Birds::Birds( const char* color, int childs, float avgLifetime, float incubation )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_incubationTime = incubation;
+};
+
+
+//Birds( ifstream& in_file );//init the Birds from a binary file
+
+
+float Birds::GetIncubationTime() const
+{
+    return m_incubationTime;
+};
+
+Fish::Fish() : m_finCount(0), m_gillsCount(0) {};//set the default color to GRAY and other params to 0
+
+Fish::Fish( const char* color, int childs, float avgLifetime, int fin, int gills )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_finCount = fin;
+    m_gillsCount = gills;
+};
+
+//Fish( ifstream& in_file );//init the Fish from a binary file
+
+int	Fish::GetFinCount() const
+{
+    return m_finCount;
+};
+
+int	Fish::GetGillsCount() const
+{
+    return m_gillsCount;
+};
+
+Horse::Horse() : m_type(NULL) {};//set the default color to GRAY and other params to 0
+
+Horse::Horse( const char* color, int childs, float avgLifetime, float preg, float milk, const char* type )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_pregnancyTime = preg;
+    m_milkLiters = milk;
+    std::strcpy(m_type, type);
+};//init the Horse with a given attributes
+
+//Horse( ifstream& in_file );//init the Horse from a binary file
+
+virtual ~Horse();
+
+const char*	Horse::GetType() const
+{
+    return m_type;
+};
+
+Flamingo::Flamingo() : m_avgHeight(0) {};//set the default color to GRAY and other params to 0
+
+
+Flamingo::Flamingo( const char* color, int childs, float avgLifetime, float incubation, float avgHeight )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_incubationTime = incubation;
+    m_avgHeight = avgHeight;
+};
+
+//Flamingo( ifstream& in_file );//init the Flamingo from a binary file
+
+virtual ~Flamingo();
+
+float Flamingo::GetHeight() const
+{
+    return m_avgHeight;
+};
+
+MammalsFish::MammalsFish() {};//set the default color to GRAY and other params to 0
+
+MammalsFish::MammalsFish( const char* color, int childs, float avgLifetime, float preg, float milk, int fin, int gills )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_pregnancyTime = preg;
+    m_milkLiters = milk;
+    m_finCount = fin;
+    m_gillsCount = gills;
+};//init the MammalsFish with a given attributes
+//MammalsFish::MammalsFish( ifstream& in_file );//init the MammalsFish from a binary file
+virtual ~MammalsFish();
+
+GoldFish::GoldFish() : m_avgWeight(0) m_avgWeight(0) {};//set the default color to GRAY and other params to 0
+GoldFish::GoldFish( const char* color, int childs, float avgLifetime, float preg, float milk, int fin, int gills, float avgW, float avgL )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_pregnancyTime = preg;
+    m_milkLiters = milk;
+    m_finCount = fin;
+    m_gillsCount = gills;
+    m_avgLength = avgL;
+    m_avgWeight = avgW;
+};
+//GoldFish::GoldFish( ifstream& in_file );//init the GoldFish from a binary file
+virtual ~GoldFish();
+	
+float GoldFish::GetWeight() const
+{
+    return m_avgWeight; 
+};//return the avg weight of the gold fish
+
+float GoldFish::GetLength() const
+{
+    return m_avgWeight;
+};
+
+
+Mermaid::Mermaid() : m_firstName(NULL), m_lastName(NULL) {};//set the default color to GRAY and other params to 0
+	
+Mermaid::Mermaid( const char* color, int childs, float avgLifetime, float preg, float milk, int fin, int gills, const char* firstName, const char* lastName )
+{
+    std::strcpy(m_color, color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+    m_pregnancyTime = preg;
+    m_milkLiters = milk;
+    m_finCount = fin;
+    m_gillsCount = gills;
+    std::strcpy(m_firstName, firstName);
+    std::strcpy(m_lastName, lastName);
+
+};//init the Mermaid with a given attributes
+//Mermaid( ifstream& in_file );//init the Mermaid from a binary file
+virtual ~Mermaid();
+	
+const char*	Mermaid::GetFirstName() const
+{
+    return m_firstName;
+};//return the first name of the mermaid
+const char*	Mermaid::GetLastName() const
+{
+    return m_lastName;
+};//return the last name of the mermaid
+
+
+Zoo::Zoo() : m_name(NULL), m_address(NULL), m_ticketPrice(0), m_openHours(NULL), m_closeHours(NULL), m_numOfAnimals(0), m_animals(NULL) {};//default c'tor  - all to 0 or null
+Zoo::Zoo( const char* name, const char* address, float ticket, const char* open, const char* close )
+{
+    std::strcpy(m_name, name);
+    std::strcpy(m_address, address);
+    m_ticketPrice = ticket;
+    std::strcpy(m_openHours, open);
+    std::strcpy(m_closeHours, close);
+};
+// Zoo( ifstream& in_file );//c'tor that gets a binary file and loads the data of the zoo from the file
+virtual ~Zoo();//d'tor
+
+const char*	Zoo::GetName() const
+{
+    return m_name;
+};//return the name of the zoo
+
+const char* Zoo::GetAddress() const
+{
+    return m_address;
+};//return the address of the zoo
+float Zoo::GetTicketPrice() const
+{
+    return m_ticketPrice;
+};//return the ticket price of the zoo
+
+const char*	Zoo::GetOpenHour() const
+{
+    return m_openHours;
+};//return the open hour of the zoo
+	
+const char*	Zoo::GetCloseHour() const
+{
+    return m_closeHours;   
+};//return the close hour of the zoo
+
+int	Zoo::GetNumOfAnimals() const
+{
+    return m_numOfAnimals;   
+};//return the num of animals in the zoo
+
+Animal** Zoo::GetAnimals() const
+{
+    return m_animals;   
+};//return the animals array of the zoo
+
+void Zoo::AddAnimal( Animal* an )
+{
+    m_an = new Animal(an.GetColor, an.GetChildCount, an.GetLifetime)
+};//creates a copy of "an" (deep copy) and adds an animal to the array
+
+public:
+	Zoo& operator+( Animal* an );//adds an animal (only pointer, no copy needed) to the class and returns this with the change
+	Zoo operator+( const Zoo& other ) const; //returns a new Zoo with the properties of this and animals of this and other (need to deep copy the data of other)
+
+public:
+	friend ofstream& operator<<( ofstream& out, const Zoo& z );//operator to write the zoo to a text file
+	friend ifstream& operator>>( ifstream& in, Zoo& z );//operator to read the zoo from a text file
+
+public:
+	void Save( ofstream& ofs ) const;//method to save the info to a text file
+	void Load( ifstream& ifs );//method to load the info from a text file
+	void SaveBin( ofstream& ofs ) const;//method to save the info to a binary file
